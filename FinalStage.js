73946079c2f685cbf5b12d92a849618e589cebfd
pashyanthi <67546604+pashyanthi@users.noreply.github.com>
2020-10-01 15:52:53 +0530

    Add files via upload

---
+++ FinalStage.js
@@ -0,0 +1,22 @@
+class nextPage {
+
+   // constructor() {
+     
+  //  }
+  display(){
+  this.Play.mousePressed(()=>{
+    this.greeting.hide()
+    this.Play.hide()
+    this.Rules.hide()
+    this.R1.hide()
+    this.R2.hide()
+    this.R3.hide()
+    this.R4.hide()
+    this.R5.hide()
+    
+    
+
+ 
+  });
+}
+}
---
+++ sketch.js
@@ -0,0 +1,24 @@
+var form;
+var canvas;
+
+function setup(){
+canvas=createCanvas(displayWidth-30,displayHeight-30)
+form= new Form()
+}
+function preload(){
+
+}
+
+
+
+
+
+function draw(){
+background("pink")
+form.display()
+}
+
+
+
+
+
